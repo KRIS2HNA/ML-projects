6b9ddb94e03b5d28741c928a80605443a0c9ba34
KRIS2HNA <krishnavamsimummdivarapu@gmail.com>
2025-06-09 23:09:41 +0530

    setup

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -9,7 +9,15 @@ def error_message_detail(error, error_detail:sys):
     return error_message 
 class CustomException(Exception):
     def __init__(self,error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message, error_detail)
     def __str__(self):
         return self.error_message
+    
+    
+if __name__ =="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("DIVIDE BY ZERO ERROR")
+        raise CustomException(e, sys) from e
